7158d45c131fbdbba908d1aa0a2c3a9f13e04e56
Steffen Vogel <post@steffenvogel.de>
2022-06-27 10:58:09 +0200

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='withings-sync',
-    version='3.4.1',
+    version='3.4.2',
     author='Masayuki Hamasaki, Steffen Vogel',
     author_email='post@steffenvogel.de',
     description='A tool for synchronisation of Withings (ex. Nokia Health Body) to Garmin Connect and Trainer Road.',
