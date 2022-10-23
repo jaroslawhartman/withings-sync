7a1f793c107580433a96871ebb81d39985c7abb3
stynoo <stijn.lenaerts@gmail.com>
2022-10-23 19:59:23 +0200

    setup.py: increase version counter

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="withings-sync",
-    version="3.4.2",
+    version="3.5",
     author="Masayuki Hamasaki, Steffen Vogel",
     author_email="post@steffenvogel.de",
     description="A tool for synchronisation of Withings (ex. Nokia Health Body) to Garmin Connect and Trainer Road.",
