64d8ec1b31c702582f00db8ea4cde24113def08f
J. Rast <juergr@gmail.com>
2023-09-28 23:21:49 +0200

    Store garth session and upload via garth

--- withings_sync/garmin.py
+++ withings_sync/garmin.py
@@ -1,8 +1,7 @@
 """This module handles the Garmin connectivity."""
 import logging
-import cloudscraper
 import garth
-
+import os
 
 log = logging.getLogger("garmin")
 
@@ -22,31 +21,34 @@ class APIException(Exception):
 class GarminConnect:
     """Main GarminConnect class"""
 
-    UPLOAD_URL = "https://connect.garmin.com/upload-service/upload/.fit"
-
-    # From https://github.com/cpfair/tapiriik
     @staticmethod
     def get_session(email=None, password=None):
-        """tapiriik get_session code"""
-        session = cloudscraper.CloudScraper()
+        logged_in = False
+        if os.path.exists('./garmin_session'):
+            garth.resume('./garmin_session')
+            try:
+                garth.client.username
+                logged_in = True
+            except Exception:
+                pass
 
-        try:
-            garth.login(email, password)
-        except Exception as ex:
-            raise APIException("Authentication failure: {}. Did you enter correct credentials?".format(ex))
+        if not logged_in:
+            try:
+                garth.login(email, password)
+                garth.save('./garmin_session')
+            except Exception as ex:
+                raise APIException("Authentication failure: {}. Did you enter correct credentials?".format(ex))
 
-        session.headers.update({'NK': 'NT', 'authorization': garth.client.oauth2_token.__str__(), 'di-backend': 'connectapi.garmin.com'})
-        return session
 
     @staticmethod
     def login(username, password):
         """login to Garmin"""
         return GarminConnect.get_session(email=username, password=password)
 
-    def upload_file(self, ffile, session):
+    def upload_file(self, ffile):
         """upload fit file to Garmin connect"""
         files = {"data": ("withings.fit", ffile)}
-        res = session.post(self.UPLOAD_URL, files=files, headers={"nk": "NT"})
+        res = garth.client.post('connect', '/upload-service/upload/.fit', files=files, api=True, headers={'di-backend': 'connectapi.garmin.com'})
         try:
             resp = res.json()
             if "detailedImportResult" not in resp:
