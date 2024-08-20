d01a6d55dac6dd6eea99524775a821c4560abd46
longstone <2110765+longstone@users.noreply.github.com>
2024-08-20 11:14:00 +0200

    bump Version to 4.2.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="withings-sync",
-    version="4.2.4",
+    version="4.2.5",
     author="Masayuki Hamasaki, Steffen Vogel",
     author_email="post@steffenvogel.de",
     description="A tool for synchronisation of Withings (ex. Nokia Health Body) to Garmin Connect and Trainer Road.",
